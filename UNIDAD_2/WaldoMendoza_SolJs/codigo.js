bcd6ae3a30a878eb6ac8b1ba4c784f070e1d90e3
Waldo Mendoza <mendozawaldos@hotmail.com>
2021-04-28 12:52:23 -0600

    Tarea y parcial

---
+++ UNIDAD_2/WaldoMendoza_SolJs/codigo.js
@@ -0,0 +1,7 @@
+/* Inicia codigo JavaScript */
+
+// Al cargarse el archivo JavaScript, se muestra un mensaje
+alert("Hola DPWEB UTEC!");
+
+// Despues del primer mensae, se muestra otro mensaje sguido 
+alert("Este es un mensaje DPWEB UTEC");
